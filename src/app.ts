fd9f1e8626f9333986b5f498c317874d3c0f96d5
Steven Brcak <sbrcak@live.com>
2024-07-12 16:02:02 -0700

    Add example response to app.ts

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,26 @@ import "express-async-errors";
 const app = express();
 app.use(express.json());
 // All code should go below this line
+//
+
+app.get("/", (_req, res) => {
+  return res.status(200).json({ message: "Hello World!" }); // the 'status' is unnecessary but wanted to show you how to define a status
+});
+
+app.get("/dogs", async (req, res) => {
+  const nameHas = req.query.nameHas as string; //makes it so the URL params can be used to fetch specified data
+  console.log({ nameHas });
+
+  const dogs = await prisma.dog.findMany({
+    where: {
+      name: {
+        contains: nameHas,
+      },
+    },
+  });
+  console.log("dogs =>", dogs);
+  return res.status(200).send(dogs);
+});
 
 // all your code should go above this line
 app.use(errorHandleMiddleware);
@@ -14,5 +34,5 @@ const port = process.env.NODE_ENV === "test" ? 3001 : 3000;
 app.listen(port, () =>
   console.log(`
 🚀 Server ready at: http://localhost:${port}
-`),
+`)
 );
